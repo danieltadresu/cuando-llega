e3701071ec2e1b369beec5e88e2b5b59a221fa0a
Nicolás Gómez <nicolasgomezespejo@gmail.com>
2020-04-20 18:53:49 -0400

    feat: use hooks

--- src/App.js
+++ src/App.js
@@ -1,77 +1,56 @@
-import React, { Component } from 'react'
+import React, { useCallback, useState } from 'react'
 import { fetchNextArrivals } from './utils'
 import LastSearch from './LastSearch'
 import ArrivalsResult from './ArrivalsResult'
 import NotFound from './NotFound'
 import SearchForm from './SearchForm'
 
-class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      stop: '',
-      arrivals: [],
-      submitting: false,
-      submittedOnce: false,
-      error: false,
-      lastUpdated: null
-    }
-    this.handleStopChange = this.handleStopChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
+function App() {
+  const [stop, setStop] = useState('');
+  const [submitting, setSubmitting] = useState(false);
+  const [submittedOnce, setSubmittedOnce] = useState(false);
+  const [error, setError] = useState(false);
+  const [arrivals, setArrivals] = useState([]);
+  const [lastUpdated, setLastUpdated] = useState();
 
-  handleStopChange (event) {
-    this.setState({ stop: event.target.value })
-  }
+  const handleSubmit = useCallback((event) => {
+    event.preventDefault();
+    setSubmitting(true);
 
-  handleSubmit (event) {
-    event.preventDefault()
-    this.setState({ submitting: true }, () => {
-      fetchNextArrivals(this.state.stop)
-        .then(arrivals =>
-          this.setState({
-            arrivals,
-            submitting: false,
-            submittedOnce: true,
-            error: false,
-            lastUpdated: new Date()
-          })
-        )
-        .catch(() =>
-          this.setState({ submitting: false, submittedOnce: true, error: true })
-        )
-    })
-  }
+    fetchNextArrivals(stop)
+      .then(arrivals => {
+        setArrivals(arrivals);
+        setSubmittedOnce(true);
+        setSubmitting(false);
+        setLastUpdated(new Date());
+        setError(false);
+      })
+      .catch(() => {
+        setSubmitting(false);
+        setSubmittedOnce(true);
+        setError(true);
+      })
+  });
 
-  render () {
-    const {
-      submitting,
-      submittedOnce,
-      error,
-      lastUpdated,
-      arrivals,
-      stop
-    } = this.state
-    return (
-      <section>
-        <article>
-          <h1 style={{ marginTop: '18px' }}>{'🚌 Cuándo llega'}</h1>
-          <SearchForm
-            stop={stop}
-            submitting={submitting}
-            onSubmit={this.handleSubmit}
-            onStopChange={this.handleStopChange}
-            buttonText={submittedOnce ? 'Actualizar' : '🔎 buscar'}
-          />
-          {error && <NotFound stop={stop} />}
-          <section id='results'>
-            {arrivals.length > 0 && <ArrivalsResult arrivals={arrivals} />}
-          </section>
-          {submittedOnce && <LastSearch lastUpdated={lastUpdated} />}
-        </article>
-      </section>
-    )
-  }
+  return (
+    <section>
+      <article>
+        <h1 style={{ marginTop: '18px' }}>{'🚌 Cuándo llega'}</h1>
+        <SearchForm
+          stop={stop}
+          submitting={submitting}
+          onSubmit={handleSubmit}
+          onStopChange={(event) => setStop(event.target.value)}
+          buttonText={submittedOnce ? 'Actualizar' : '🔎 buscar'}
+        />
+        {error && <NotFound stop={stop} />}
+        <section id='results'>
+          {arrivals.length > 0 && <ArrivalsResult arrivals={arrivals} />}
+        </section>
+        {submittedOnce && <LastSearch lastUpdated={lastUpdated} />}
+      </article>
+    </section>
+  )
 }
 
 export default App
--- src/LastSearch.js
+++ src/LastSearch.js
@@ -1,48 +1,27 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
-class LastSearch extends Component {
-  static getDerivedStateFromProps (props, state) {
-    if (
-      props.lastUpdated &&
-      props.lastUpdated.getTime() !== state.lastUpdated.getTime()
-    ) {
-      return {
-        lastUpdated: props.lastUpdated,
-        seconds: 0
-      }
+function LastSearch({ lastUpdated }) {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    setSeconds(0);
+  }, [lastUpdated]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
     }
-    return null
-  }
+  }, []);
 
-  constructor (props) {
-    super(props)
-    this.state = {
-      seconds: 0,
-      lastUpdated: props.lastUpdated || new Date()
-    }
-    this.tick = this.tick.bind(this)
-  }
-
-  componentDidMount () {
-    this.intervalHandle = setInterval(this.tick, 1000)
-  }
-
-  componentWillUnmount () {
-    clearInterval(this.intervalHandle)
-  }
-
-  tick () {
-    this.setState(state => ({ seconds: state.seconds + 1 }))
-  }
-
-  render () {
-    return (
-      <i>
-        <small>Resultados obtenidos hace {this.state.seconds} segundos</small>
-      </i>
-    )
-  }
+  return (
+    <i>
+      <small>Resultados obtenidos hace {seconds} segundos</small>
+    </i>
+  )
 }
 
 LastSearch.propTypes = {
